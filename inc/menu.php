d8b850df2849fcac979be65bb9b5381c6a69299b
Dick BkD <bkd@hoornbeeck.nl>
2026-06-02 15:54:57 +0200

    Geen rechten/rollen meer

--- inc/menu.php
+++ inc/menu.php
@@ -7,36 +7,18 @@ $inlognaam=isset($_SESSION['inlognaam']) ? $_SESSION['inlognaam'] : '';
 // samenstellen menu
 $menu='';
 // op basis van rol menu tonen
-switch ($autRol){
-    case 'bedrijfsleider':
-        $menu= '<nav>
-
-               <ul>
-                    <li><a class="navbar-brand" href="kassa.php"><img src="img/logo_buskruit.png" class="img-responsive" alt="home..."/></a></li><!-- Logo -->
-                    <li><a href="voorraad.php">Voorraad</a></li>
-                    <li><a href="voorraad_import.php">Importeren</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="uitloggen.php">Uitloggen</a></li>
-                </ul>
-              </nav>';
-        break;
-    case 'medewerker':
-        $menu='<nav>
-               <ul>
-                    <li><a href="kassa.php">Home</a></li>
-                    <li><a href="uitloggen.php">Uitloggen</a></li>
-                </ul>
-               </nav>';
-        break;
-    default:
-        $menu='<nav>
-               <ul>
-                     <li><a href="uitloggen.php">Uitloggen</a></li>
-                </ul>
-               </nav>';
+    $menu = '<nav>
+                   <ul>
+                        <li><a class="navbar-brand" href="kassa.php"><img src="img/logo_buskruit.png" class="img-responsive" alt="home..."/></a></li><!-- Logo -->
+                        <li><a href="voorraad.php">Voorraad</a></li>
+                        <li><a href="voorraad_import.php">Importeren</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="uitloggen.php">Uitloggen</a></li>
+                   </ul>
+                </nav>';
+if ($ingelogd) {
+    echo $menu;
 }
-
-echo $menu;
 
 ?>
 
